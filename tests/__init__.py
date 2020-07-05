16b54a1875157449333bebce671640b9eb24b92c
Sratege <213346@supinfo.com>
2020-07-05 19:08:20 +0200

    correction

--- tests/__init__.py
+++
@@ -1,8 +0,0 @@
-import unittest
-
-
-# c.get("/", headers={"Authorization": "Basic {}".format(base64.b64encode(b"useo@hotmail.com:pass").decode("utf8"))})
-#rv = client.get("/app/messages", headers={"Authorization": f"Basic {auth_str}"})
-if __name__ == '__main__':
-    unittest.main()
-
